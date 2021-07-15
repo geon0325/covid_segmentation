57d9b57ebf577f772599d1c98569fcf51bd01e09
Geon Lee <lg970325@naver.com>
2021-07-15 19:32:35 +0900

    Update learn_nl.py

--- code/learn_nl.py
+++ code/learn_nl.py
@@ -10,7 +10,7 @@ d = 2
 
 def write_result(filename, X, DIST, BOUND, mean=None, std=None):
     (n, d) = np.shape(X)
-
+    
     dist_comb = DIST[0]
     for i in range(1, len(DIST)):
         dist_comb = np.concatenate((dist_comb, DIST[i]))
@@ -35,8 +35,8 @@ def write_result(filename, X, DIST, BOUND, mean=None, std=None):
         f.write(f_str[:-1] + '\n')
     f.close()
 
-def _train_nl(X, linearFit=False):
-    model = NLDS(X)
+def _train_nl(X, k=2, linearFit=False):
+    model = NLDS(X, k)
     model = model.fit(wtype='uniform', linearfit=linearFit)
     return model
 
@@ -58,14 +58,14 @@ def get_costC(X_org, X_rec, std=-1):
     costC = np.nansum(-np.log2(pdfs))
     return costC, std
 
-def segment(X, BOUND, linearFit, N, max_bound=-1e10):
+def segment(X, BOUND, linearFit, N, k, max_bound=-1e10):
     s = BOUND[0]; e = BOUND[1];
     (n, d) = np.shape(X[s:e])
     
     # without segmentation
-    model = _train_nl(X[s:e], linearFit=linearFit)
+    model = _train_nl(X[s:e], k, linearFit=linearFit)
     dist = _get_dist(model, linearFit=linearFit)[1]
-    costM = C_F * (model.K**2 + (d+2+int(not linearFit))*model.K + d)
+    costM = C_F * (model.k**2 + (d+2+int(not linearFit))*model.k + d)
     costC, std = get_costC(X[s:e], dist)
     costT = costM + costC
     
@@ -80,12 +80,12 @@ def segment(X, BOUND, linearFit, N, max_bound=-1e10):
             if s + i > max_bound: continue
 
         try:
-            model_1 = _train_nl(X[s1:e1], linearFit=linearFit)
+            model_1 = _train_nl(X[s1:e1], k, linearFit=linearFit)
             dist_1 = _get_dist(model_1, linearFit=linearFit)[1]
             costM_1 = costM
             costC_1, std_1 = get_costC(X[s1:e1], dist_1, std)
 
-            model_2 = _train_nl(X[s2:e2], linearFit=linearFit)
+            model_2 = _train_nl(X[s2:e2], k, linearFit=linearFit)
             dist_2 = _get_dist(model_2, linearFit=linearFit)[1]
             costM_2 = costM
             costC_2, std_2 = get_costC(X[s2:e2], dist_2, std)
@@ -98,29 +98,30 @@ def segment(X, BOUND, linearFit, N, max_bound=-1e10):
                 min_BOUND = [[s1, e1], [s2, e2]]
                 min_DIST = [dist_1, dist_2]
                 min_MODEL = [model_1, model_2]
-        except: continue
+        except:
+            continue
 
     if min_costT == costT:
         return min_DIST, min_BOUND, min_MODEL
     else:
-        min_DIST_1, min_BOUND_1, min_MODEL_1 = segment(X, min_BOUND[0], linearFit, N)
-        min_DIST_2, min_BOUND_2, min_MODEL_2 = segment(X, min_BOUND[1], linearFit, N)
+        min_DIST_1, min_BOUND_1, min_MODEL_1 = segment(X, min_BOUND[0], linearFit, N, k)
+        min_DIST_2, min_BOUND_2, min_MODEL_2 = segment(X, min_BOUND[1], linearFit, N, k)
         return min_DIST_1 + min_DIST_2, min_BOUND_1 + min_BOUND_2, min_MODEL_1 + min_MODEL_2
 
-def fit_data(X, fn, linearFit, mean, std):
+def fit_data(X, fn, linearFit, mean, std, k):
     #----------------------------------#
     tl.comment("Start learning NL")
     #----------------------------------#
     _path = fn + 'fit_data'
     (n, d) = np.shape(X);
     linearFit = linearFit
-    
-    DIST, BOUND, MODEL = segment(X, [0, n], linearFit, n)
+
+    DIST, BOUND, MODEL = segment(X, [0, n], linearFit, n, k)
     BOUND = [[BOUND[i],BOUND[i+1]] for i in range(0, len(BOUND), 2)]
     if not linearFit: write_result(_path + '.txt', X, DIST, BOUND, mean, std)
     else: write_result(_path + '_lin.txt', X, DIST, BOUND, mean, std)
 
-def forecast_data(X, fn, linearFit, mean, std):
+def forecast_data(X, fn, linearFit, mean, std, k):
     #----------------------------------#
     tl.comment("Start learning NL")
     #----------------------------------#
@@ -132,7 +133,7 @@ def forecast_data(X, fn, linearFit, mean, std):
     train_size = int(n * alpha)
     test_size = n - train_size
 
-    DIST, BOUND, MODEL = segment(X, [0, train_size], linearFit, train_size, max_bound=train_size-test_size)
+    DIST, BOUND, MODEL = segment(X, [0, train_size], linearFit, train_size, k, max_bound=train_size-test_size)
     BOUND = [[BOUND[i],BOUND[i+1]] for i in range(0, len(BOUND), 2)]
     DIST[-1] = _get_dist(MODEL[-1], linearFit=linearFit, dist_len=len(DIST[-1])+test_size)[1]
     if not linearFit: write_result(_path + '.txt', X, DIST, BOUND, mean, std)
@@ -142,7 +143,7 @@ def forecast_data(X, fn, linearFit, mean, std):
     tl.comment("Finish learning NL")
     #----------------------------------#
 
-def fit_data_single(X, fn, linearFit, mean, std):
+def fit_data_single(X, fn, linearFit, mean, std, k):
     #----------------------------------#
     tl.comment("Start learning NL")
     #----------------------------------#
@@ -150,7 +151,7 @@ def fit_data_single(X, fn, linearFit, mean, std):
     (n, d) = np.shape(X);
     linearFit = linearFit
     
-    model = _train_nl(X, linearFit=linearFit)
+    model = _train_nl(X, k, linearFit=linearFit)
     dist = _get_dist(model, linearFit=linearFit)[1]
     DIST = [dist]
     BOUND = [[0, n]]
@@ -161,8 +162,8 @@ def fit_data_single(X, fn, linearFit, mean, std):
     #----------------------------------#
     tl.comment("Finish learning NL")
     #----------------------------------#
-   
-def forecast_data_single(X, fn, linearFit, mean, std):
+
+def forecast_data_single(X, fn, linearFit, mean, std, k):
     #----------------------------------#
     tl.comment("Start learning NL")
     #----------------------------------#
@@ -174,7 +175,7 @@ def forecast_data_single(X, fn, linearFit, mean, std):
     train_size = int(n * alpha)
     test_size = n - train_size
 
-    model = _train_nl(X[:train_size], linearFit=linearFit)
+    model = _train_nl(X[:train_size], k, linearFit=linearFit)
     dist = _get_dist(model, linearFit=linearFit, dist_len=n)[1]
     DIST = [dist]
     BOUND = [[0, train_size]]
@@ -185,9 +186,8 @@ def forecast_data_single(X, fn, linearFit, mean, std):
     #----------------------------------#
     tl.comment("Finish learning NL")
     #----------------------------------#
- 
 
-def fit_data_inc(X, fn, ERR_RAT, linearFit, mean, std):
+def fit_data_inc(X, fn, ERR_RAT, linearFit, mean, std, k):
     #----------------------------------#
     tl.comment("Start learning NL")
     #----------------------------------#
@@ -204,7 +204,7 @@ def fit_data_inc(X, fn, ERR_RAT, linearFit, mean, std):
         X_size = tl.NORM(X[s_idx:e_idx], [])
 
         try:
-            model = _train_nl(X[s_idx:e_idx], linearFit=linearFit)
+            model = _train_nl(X[s_idx:e_idx], k, linearFit=linearFit)
             dist = _get_dist(model, linearFit=linearFit)[1]
             err = tl.NORM(X[s_idx:e_idx], dist)
             print(s_idx, '\t', e_idx, '\t', err, '\t', X_size * ERR_RAT)
@@ -231,7 +231,7 @@ def fit_data_inc(X, fn, ERR_RAT, linearFit, mean, std):
     e_idx = n
     while True:
         try:
-            model = _train_nl(X[s_idx:e_idx], linearFit=linearFit)
+            model = _train_nl(X[s_idx:e_idx], k, linearFit=linearFit)
             dist = _get_dist(model, linearFit=linearFit)[1]
             DIST.append(dist)
             BOUND.append([s_idx, e_idx])
